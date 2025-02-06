4e9c87578e7732451b03c73f7bb72aefe809802f
Gabriel Silva <gabriel.silva.ext@einstein.br>
2025-02-06 19:29:36 -0300

    Refactor sidebar layout handling in Page and RootLayout.
    Moved `SidebarProvider`, `AppSidebar`, and `SidebarInset` from `Page` to `RootLayout` to centralize sidebar management. Simplifies `Page` component by removing unnecessary dependencies and ensures consistent sidebar behavior across the app.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,26 +2,32 @@ import type { Metadata } from "next";
 import "../styles/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Footer } from "@/components/footer";
+import type { Layout } from "@/interfaces";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 
 export const metadata: Metadata = {
-	title: "Nestjs Decorated Dataloaders",
+	title: "Decorated Dataloaders",
 	description: "Documentation for nestjs-decorated-dataloaders package",
 	icons: {
 		icon: "/white-rabbit.svg",
 	},
 };
 
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Layout) {
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<body>
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 					<div className="flex min-h-screen flex-col">
-						<main className="flex-1">{children}</main>
+						<main className="flex-1">
+							<SidebarProvider>
+								<AppSidebar />
+								<SidebarInset>
+									{children}
+								</SidebarInset>
+							</SidebarProvider>
+						</main>
 						<Footer />
 					</div>
 				</ThemeProvider>
